12a308a9f0e6f6386e275a08489e910be2315108
Jorge Carrillo <rack1992@gmail.com>
2025-04-16 17:57:33 -0500

    Se crea la rama de QR Generator

--- src/utils/GenerateIdQR.js
+++ src/utils/GenerateIdQR.js
@@ -1,25 +1,44 @@
-import crypto from "crypto";
+const ByteLegnth = 16;
+const CountListQR = 10;
 
-const byteLegnth = 16;
+let _listContainer;
+let _listQRImgContainer;
 
 function ConfigHTML() {
   document.getElementById("GenerarQR").addEventListener(`click`, () => {
-    GenerarListaQR(2);
+    GenerarListaQR(CountListQR);
   });
+
+  _listContainer = document.getElementById("QrList_Container");
+  _listQRImgContainer = document.getElementById("QrLIstImag_Container");
+
+  new QRCode(document.getElementById("qrcode"), "uniqueId");
 }
 
 function generateUniqueHexString() {
-  return crypto.randomBytes(byteLegnth).toString("hex");
+  const id = crypto.randomUUID();
+  console.log(id); // Ejemplo: '3f9f1b6e-8f4c-4a2f-9c3e-1d2b3a4c5d6e'
+  return id;
 }
 
 function GenerarListaQR(count) {
-  console.log("Funciona");
-  console.log(count);
+  for (let i = 0; i < count; i++) {
+    GenerarQR();
+  }
 }
 
 function GenerarQR() {
   const uniqueId = generateUniqueHexString();
-  new QRCode(document.getElementById("qrcode"), uniqueId);
+  const liElement = document.createElement("li");
+  const imgElement = document.createElement("div");
+
+  imgElement.setAttribute("id", uniqueId);
+
+  liElement.innerText = uniqueId;
+  _listContainer.appendChild(liElement);
+  _listQRImgContainer.appendChild(imgElement);
+
+  new QRCode(imgElement, uniqueId);
 }
 
 ConfigHTML();
